c485e8afd1f3a76dce8a2c0036e8527a86628835
Harrison Malone <harrison.malone2@gmail.com>
2019-08-28 22:22:20 +1000

    added human class

---
+++ 04-human-class.rb
@@ -0,0 +1,11 @@
+# 1. Create a Human class
+
+# 2. Give the Human class some instance variables; eye_colour, hair_colour, height
+
+# 3. We want to be able to READ eye_colour, hair_colour and height, and WRITE hair_colour
+
+# 4. Create two classes, Hippy and Influencer, that inherit from Human, except an Influencer needs a shower method, whilst the Hippy needs a fire_twirl method
+
+# 5. Seperate these into seperate files
+
+# 6. If you're feeling wild, find a cool speech gem, that you can add to the Human class
