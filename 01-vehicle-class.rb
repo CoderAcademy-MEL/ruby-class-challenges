03e227acd98f5fe3bbad510f0fc5c078d8379b51
Harrison Malone <harrison.malone2@gmail.com>
2019-08-28 21:52:00 +1000

    initial commit

---
+++ 01-vehicle-class.rb
@@ -0,0 +1,21 @@
+# 1. Implement a Vehicle class. It should accept make and model properties when you create an object.
+# => civic = Vehicle.new("Honda", "Civic")
+
+# 2. Implement a Car class. It should inherit from Vehicle.
+# => civic = Car.new("Honda", "Civic")
+
+# 3. Implement a Motorbike class. It should also inherit from Vehicle.
+# => low_rider = Motorbike.new("Harley Davidson", "Low Rider")
+
+# 4. Implement the following methods, but first, device where they should live.
+
+# a. refuel (should take one argument: litres)
+
+# b. wheelie (should display a message)
+
+# c. wind_up_windows (should display a message)
+
+# d. fuel_level (should display total litres in the tank)
+
+# 5. A car should only allow maximum 50L of petrol and a bike 15L. Keep your code DRY!
+# Hint: research how super works.
